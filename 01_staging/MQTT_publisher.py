06f14c80633f3d48ed3c134b7ed8a7ba3ef53836
JoDi-2903 <jonathan.diebel@web.de>
2022-11-04 18:46:08 +0100

    finished tasks of week 2

--- 01_staging/MQTT_publisher.py
+++ 01_staging/MQTT_publisher.py
@@ -12,24 +12,20 @@ import random
 import string
 import json
 
-# Connect to MQTT client
-broker_address="broker.hivemq.com"
-client = mqtt.Client("inf20026", clean_session=False) #use your own unique ID
-client.connect(broker_address)
 
 # Function to generate a random FIN
 def random_fin_generator():
-    fin_anfang_liste = ["DD",'WVW','WBA','WB1','1FM','ZFF','WMA','WAU','WPO','SCC','WOL','VF1',"SNT"]
-    fin_anfang = random.choice(fin_anfang_liste)
-    fin_ende = ''.join(random.choices(string.ascii_uppercase + string.digits, k=14))
-    random_fin = fin_anfang+fin_ende
+    fin_producer_list = ["DD",'WVW','WBA','WB1','1FM','ZFF','WMA','WAU','WPO','SCC','WOL','VF1',"SNT"]
+    fin_start = random.choice(fin_producer_list)
+    fin_end = ''.join(random.choices(string.ascii_uppercase + string.digits, k=14))
+    random_fin = fin_start + fin_end
     return random_fin
 
 # Function to generate json
 def generate_json():
 
     data = {
-        "fin": "WPOSJYDU7SDO692GB",
+        "fin": FIN if use_random_FIN == False else random_fin_generator(),
         "zeit": datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S.%f"),
         "geschwindigkeit": random.randint(0, 200),
         "ort": random.randint(1, 10)
@@ -39,11 +35,26 @@ def generate_json():
 
 # MAIN
 if __name__ == "__main__":
-    # Publish random JSON every 5 seconds
-    while True:
-        generated_json = generate_json()
-        client.publish("DataMgmt/FIN", generated_json, qos=1)
-        print(generated_json)
-        time.sleep(5)
+    # Configure parameters
+    broker_address="broker.hivemq.com"
+    FIN = "WPOSJYDU7SDO692GB"
+    use_random_FIN = False
 
+    # Connect to MQTT client
+    client = mqtt.Client("inf20026_publisher", clean_session=False) #use your own unique ID
+    client.connect(broker_address)
 
+    # Loop until Ctrl-C is pressed
+    try:
+        # Publish random JSON every 5 seconds
+        while True:
+            generated_json = generate_json()
+            client.publish("DataMgmt/FIN", generated_json, qos=1)
+            print(generated_json)
+            time.sleep(5)
+    except KeyboardInterrupt:
+        pass
+
+    # Close connection to MQTT client
+    client.disconnect()
+    print("Connection closed successfully.")
--- 01_staging/MQTT_subscriber.py
+++ 01_staging/MQTT_subscriber.py
@@ -4,29 +4,62 @@
  Autor:            Jonathan Diebel
  Erstelldatum:     04.11.2022
 """
+
 import paho.mqtt.client as mqtt
+import psycopg2
 import json
 from json import JSONDecodeError
 
+
+# Connect to postgres database
+postgres = psycopg2.connect("dbname='staging' user='postgres' password='123456' host='localhost' port='5432'")
+postgres.autocommit = True
+cursor = postgres.cursor()
+
+# Receive message and process it
 def on_message(client, userdata, message):
     data = message.payload.decode("utf-8")
 
     try:
         data = json.loads(data)
-            if isinstance(data, dict):
-                
-
+        if isinstance(data, dict):
+                if data["fin"] == FIN or read_all_messages == True:
+                    print(f"Received Message: {data}")
+                    if write_message_to_postgres == True:
+                        write_message_to_postgres(data)
     except JSONDecodeError:
         print("Error while decoding JSON")
         pass
     except Exception as err:
         print(f"Unhandled error {err}")
 
-# Connect to MQTT client and subscribe to "DataMgmt/FIN"
-broker_address="broker.hivemq.com"
-client = mqtt.Client("inf20026", clean_session=False) #use your own unique ID
-client.on_message = on_message
-client.connect(broker_address)
-client.subscribe("DataMgmt/FIN", qos=1)
-client.loop_forever()
+# Write message to postgres database
+def write_message_to_postgres(data):
+    cursor.execute(f"INSERT INTO staging.kfzzuordnung (payload, erstellt_am, quelle) VALUES ('{str(data)}', '{data['zeit']}', 'MQTT');")
 
+
+# MAIN
+if __name__ == '__main__':
+    # Configure parameters
+    broker_address="broker.hivemq.com"
+    FIN = "WPOSJYDU7SDO692GB"
+    read_all_messages = True
+    write_messages_to_postgres = True
+
+    # Connect to MQTT client and subscribe to "DataMgmt/FIN"
+    client = mqtt.Client("inf20026_subscriber", clean_session=False) #use your own unique ID
+    client.on_message = on_message
+    client.connect(broker_address)
+    client.subscribe("DataMgmt/FIN", qos=1)
+
+    # Loop until Ctrl-C is pressed
+    try:
+        client.loop_forever()
+    except KeyboardInterrupt:
+        pass
+
+    # Close connections to MQTT client and postgres
+    client.disconnect()
+    #cursor.close()
+    #postgres.close()
+    print("All connections closed successfully.")
