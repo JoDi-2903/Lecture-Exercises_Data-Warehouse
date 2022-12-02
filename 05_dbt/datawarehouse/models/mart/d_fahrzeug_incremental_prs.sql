83fa68da38f225535c216af0bccd26e38fac9b24
Jonathan Diebel <jonathan.diebel@web.de>
2022-12-02 14:48:44 +0100

    worked on task 5

---
+++ 05_dbt/datawarehouse/models/mart/d_fahrzeug_incremental_prs.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+with w_fahrzeug as (
+    SELECT staging.fahrzeug.fin, staging.fahrzeug.baujahr, staging.fahrzeug.modell, 
+    staging.hersteller.hersteller, staging.kfzzuordnung.kfz_kennzeichen from staging.fahrzeug
+    inner join staging.hersteller on staging.hersteller.hersteller_code = staging.fahrzeug.hersteller_code
+    inner join staging.kfzzuordnung on staging.kfzzuordnung.fin = staging.fahrzeug.fin
+)
+select ROW_NUMBER () OVER () as d_fahrzeug_key, f.fin, f.baujahr, f.modell, f.hersteller, f.kfz_kennzeichen
+from w_fahrzeug f
